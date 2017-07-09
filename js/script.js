8d7818b9cce0c947c2f505ff0b383aa5473deabe
victor-iyiola <javafolabi@gmail.com>
2017-07-09 12:56:45 +0100

    Quiz algorithm

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function submitAnswers() {
+  const total = 5
+  let score = 0
+  // get user input
+  const q1 = document.forms['quiz-form']['q1'].value
+  const q2 = document.forms['quiz-form']['q2'].value
+  const q3 = document.forms['quiz-form']['q3'].value
+  const q4 = document.forms['quiz-form']['q4'].value
+  const q5 = document.forms['quiz-form']['q5'].value
+
+  // validation
+  for ( let i=1; i<=total; i++ )
+    if ( !eval('q'+i) )
+      alert(`You missed question ${i}`)
+
+  // // set correct answers
+  const answers = ['b', 'a', 'd', 'b', 'd'];
+  // // Check answers
+  for ( let i=0; i<total; i++)
+    if ( eval(`q${i+1}`) === answers[i] )
+      score++;
+
+  alert(score)
+
+  return false
+}
